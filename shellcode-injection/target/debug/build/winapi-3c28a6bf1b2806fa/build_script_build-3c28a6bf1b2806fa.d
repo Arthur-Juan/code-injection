bcc3438abcdd08a3b011bb576917f13b626feeef
Arthur Lima <arthur.lima@inforce.com.br>
2024-11-13 22:45:12 -0300

    initial

---
+++ shellcode-injection/target/debug/build/winapi-3c28a6bf1b2806fa/build_script_build-3c28a6bf1b2806fa.d
@@ -0,0 +1,5 @@
+C:\projects\temp\code-injection\shellcode-injection\target\debug\build\winapi-3c28a6bf1b2806fa\build_script_build-3c28a6bf1b2806fa.exe: C:\Users\arthu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+C:\projects\temp\code-injection\shellcode-injection\target\debug\build\winapi-3c28a6bf1b2806fa\build_script_build-3c28a6bf1b2806fa.d: C:\Users\arthu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
+
+C:\Users\arthu\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
